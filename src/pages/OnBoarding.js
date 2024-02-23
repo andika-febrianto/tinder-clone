f630283fb53c278bc83b2a8d39845cafc84c6ad8
andika febrianto <andika_f@yahoo.com>
2024-02-23 17:12:55 +0700

    onboarding input

--- src/pages/OnBoarding.js
+++ src/pages/OnBoarding.js
@@ -2,11 +2,27 @@ import React from 'react'
 import Nav from '../components/Nav'
 
 const OnBoarding = () => {
+
+  const handleSubmit =()=>{
+
+  }
+
   return (
     <>
      <Nav minimal={true} 
            setShowModal={()=>{}} 
            setIsSignUp ={false}/>
+      
+      <div className='onboarding'>
+        <h2>CREATE ACCOUNT</h2>
+
+        <form onSubmit={handleSubmit}>
+          <section>
+            <label>First Name</label>
+            
+          </section>
+        </form>
+      </div>
     </>
   )
 }
