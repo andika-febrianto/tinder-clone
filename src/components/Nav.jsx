d1dcbb84be16cd81481b1340a6f6ec2b47fe5c03
andika febrianto <andika_f@yahoo.com>
2024-02-23 08:34:19 +0700

    Code AuthModal

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,14 +3,22 @@ import whiteLogo from '../images/tinder-logo-white.png'
 import colorLogo from '../images/Tinder-Logo-red.png'
 
 
-const Nav = ({minimal, authToken}) => {
+const Nav = ({ minimal, authToken, setShowModal, showModal }) => {
+
+  const handleClick =()=>{
+    setShowModal(true)
+  }
 
   return (
     <nav>
       <div className='logo-container'>
         <img className='logo' src={minimal ? colorLogo : whiteLogo} alt='' />
       </div>
-      {!authToken && !minimal && <button className='nav-button'>Log in</button>}
+      {!authToken && !minimal && 
+      <button className='nav-button'
+        onClick={handleClick}
+        disabled={showModal}
+      >Log in</button>}
     </nav>
   )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,22 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Nav from '../components/Nav'
-
+import AuthModal from '../components/AuthModal'
 
 const Home = () => {
   const outhToken = false
+  const [showModal, setShowModal] = useState(false)
 
   const handleClick =()=>{
     console.log('clicked')
+    setShowModal(true)
   }
 
   return (
     <div className='overlay'>
-      <Nav minimal={false} authToken={outhToken}/>
+      <Nav minimal={false} authToken={outhToken} setShowModal={setShowModal} showModal={showModal}/>
       <div className='home'>
         <h1>Swipe Right@</h1>
         <button className='primary-button' onClick={handleClick}>
           {outhToken ? 'SignOut': 'Create Account'}
         </button>
+
+        {showModal && <AuthModal  setShowModal={setShowModal}/>}
       </div>
     </div>
   )
